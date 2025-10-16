0d80582e8c4801bba4327dd4d4eb35086dc0e5bb
zxcpisya <mamin.papa0@gmail.com>
2025-10-16 17:29:21 +1000

    more tests, add e output

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gauss_method import gauss_method
 from optimal_exclusion_method import optimal_exclusion_method
 
+import numpy as np
+
 def main(): 
     matrix = [
         [5, 2, 3, 3],
@@ -13,17 +15,41 @@ def main():
         [2, 4, 1, 1], 
     ]
 
+    matrix_3 = [[1, -1, 4, 3, 1, 6],
+        [-1, 4, 6, 2, 1, 7],
+        [-1, 2, 3, 2, 3, 8],
+        [1, 1, -4, 2, 2, 9],
+        [-2, -1, 1, 1, 10, 10]
+    ]
+
+    matrix_4 = [[1, -1, 4, 3, 1, 6, 2],
+            [-1, 4, 6, 2, 1, 7, -5],
+            [-1, 2, 3, 2, 3, 8, 2],
+            [1, 1, -4, 2, 2, 9, 7],
+            [-2, -1, 1, 1, 10, 10, 15],
+            [-6, 7, 3, 0, -4, 2, 6]
+            ]
+
+
     matrixs = [
         matrix,
-        matrix1
+        matrix1, 
+        matrix_3, 
+        matrix_4
     ]
 
     for m in matrixs: 
-        print("matrix", m)
-        print("optimal exclusion method")
-        print(optimal_exclusion_method(m))
-        print("gauss method")
-        print(gauss_method(m))
+        A = [s[:-1] for s in m]
+        B = [s[-1] for s in m]
+
+        X = optimal_exclusion_method(m)
+        e = np.dot(A, X) - B
+
+        print(e)
+
+        print()
+
+
 
     print()
 
