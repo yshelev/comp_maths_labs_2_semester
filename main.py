6a5e3020eadbf1930ebc34cef89fd383fe5e0bc4
zxcpisya <mamin.papa0@gmail.com>
2025-12-12 10:45:27 +1000

    123

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ A = np.array([
     [-2, 3, 6, 2, 0],
     [0, -1, 2, 4, -1],
     [1, 2, 0, -1, 3]])
-
+A = np.array([
+    [1, 2], 
+    [2, 1]    
+])
 lambdas = rotation_method_with_obstacles(A)
 
 print("my lambdas: ")
@@ -18,3 +21,5 @@ print("np lambdas: ")
 numpy_lambdas = np.linalg.eigvals(A)
 for lambda_ in sorted(numpy_lambdas): 
     print(lambda_)
+    
+print(max(abs(l1 - l2) for l1, l2 in zip(lambdas, numpy_lambdas)))
